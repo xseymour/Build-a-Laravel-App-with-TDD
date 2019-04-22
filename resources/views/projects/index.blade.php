5bc41a1be537d538686dcc848c399d9cf1690823
Xavier Seymour <xseymour3@gmail.com>
2019-04-22 15:31:08 -0400

    10. Grid Spacing and Card Tweaks
    Let's continue working on the CSS for the projects dashboard. This will give us an opportunity to discuss grid spacing, and how we can use a combination of padding and negative margins to perfectly align our columns.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-	{{--Subheader--}}
-	<div class="flex items-center mb-3">
-		<a href="/projects/create">New Project</a>
-	</div>
+
+	<header class="flex justify-between items-center mb-3 py-4">
+		<h2 class="font-normal text-sm text-grey no-underline">My Projects</h2>
+		<a class="button" href="/projects/create">New Project</a>
+	</header>
 
 	{{--Birdboard Project Cards--}}
-	<div class="flex">
+	<main class="lg:flex lg:flex-wrap -mx-3">
 		@forelse($projects as $project)
-			<div class="bg-white mr-4 rounded shadow p-5 w-1/3" style="height: 200px;">
-				<h3 class="font-normal text-xl py-4">{{$project->title}}</h3>
-				<div class="text-grey">{{str_limit($project->description, 100)}}</div>
+			<div class="lg:w-1/3 px-3 pb-6">
+				<div class="bg-white rounded-lg shadow p-5" style="height: 200px;">
+					<a href="{{$project->path()}}" class="text-black no-underline">
+						<h3 class="font-normal text-xl py-4 mb-3 -ml-5 pl-4 border-l-4 border-blue-light">{{$project->title}}</h3>
+					</a>
+					<div class="text-grey">{{str_limit($project->description, 100)}}</div>
+				</div>
 			</div>
 		@empty
 			<div>No Projects Yet.</div>
 		@endforelse
-	</div>
+	</main>
 
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Build A Laravel App With TDD</title>
+@extends('layouts.app')
 
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <style type="text/css">body {padding-top: 40px !important}</style>
-    </head>
-    <body>
-        <div id="app" class="container">
-            <a :href="laracast_url"  target="_blank">Series List</a>
-            <br><br>
-            <example></example>
-        </div>
-
-        {{-- Scripts --}}
-        <script src="{{ mix('js/manifest.js') }}"></script>
-        <script src="{{ mix('js/vendor.js') }}"></script>
-        <script src="{{ mix('js/app.js') }}"></script>
-    </body>
-</html>
+@section('content')
+    <a :href="laracast_url" target="_blank">Series List</a>
+@endsection
