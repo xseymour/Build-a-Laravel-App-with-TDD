457ccf1504c342e6e645a68824005c684ddac641
Xavier Seymour <xseymour3@gmail.com>
2019-04-22 14:21:15 -0400

    9. Go Go Gadget Tailwind
    In this episode, we'll leverage Tailwind to begin constructing the Birdboard UI.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts.app')
+
 @section('content')
+	{{--Subheader--}}
 	<div class="flex items-center mb-3">
 		<a href="/projects/create">New Project</a>
 	</div>
-	<ul>
+
+	{{--Birdboard Project Cards--}}
+	<div class="flex">
 		@forelse($projects as $project)
-			<li><a href="{{$project->path()}}">{{$project->title}}</a></li>
+			<div class="bg-white mr-4 rounded shadow p-5 w-1/3" style="height: 200px;">
+				<h3 class="font-normal text-xl py-4">{{$project->title}}</h3>
+				<div class="text-grey">{{str_limit($project->description, 100)}}</div>
+			</div>
 		@empty
-			No Projects Yet.
+			<div>No Projects Yet.</div>
 		@endforelse
-	</ul>
+	</div>
+
+
 @endsection
